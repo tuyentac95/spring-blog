6f073f0d506f1c07746274c80adc302c7744f887
tuyentac95 <tuyentac95@gmail.com>
2020-09-29 20:36:32 +0700

    read and write blog's content to file

--- src/main/java/com/codegym/controller/BlogController.java
+++ src/main/java/com/codegym/controller/BlogController.java
@@ -8,16 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.*;
 
 @Controller
 public class BlogController {
+    public static final String CONTENT_SOURCE = "E:\\CodeGym\\SpringMVC\\SpringBlog\\src\\main\\webapp";
     @Autowired
     private BlogService blogService;
 
@@ -45,21 +43,46 @@ public class BlogController {
     }
 
     @PostMapping("/create-blog")
-    public ModelAndView testInput(@ModelAttribute("blog") Blog blog,@RequestParam("content-html") String content) throws IOException {
+    public ModelAndView createBlog(@ModelAttribute("blog") Blog blog,@RequestParam("content-html") String content) throws IOException {
         String content_id = blog.getTitle().replaceAll("\\s+","-").toLowerCase();
 
         blog.setContent_id(content_id);
         blogService.save(blog);
 
-        File file = new File("WEB-INF\\content\\test.txt");
+        File file = new File(CONTENT_SOURCE + "\\WEB-INF\\content\\"+content_id+".txt");
 
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
         bufferedWriter.write(content);
         bufferedWriter.close();
 
-
         ModelAndView modelAndView = new ModelAndView("/blog/test");
         modelAndView.addObject("content",content);
         return modelAndView;
     }
+
+    @GetMapping("/view-blog/{id}")
+    public ModelAndView showBlog(@PathVariable Long id) throws IOException {
+        ModelAndView modelAndView;
+        Blog blog = blogService.findById(id);
+        if(blog != null){
+           modelAndView  = new ModelAndView("/blog/show");
+           modelAndView.addObject("blog",blog);
+
+           String content_id = blog.getContent_id();
+           String content = "";
+           File file = new File(CONTENT_SOURCE + "\\WEB-INF\\content\\"+content_id+".txt");
+           BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+           String text;
+           while ((text = bufferedReader.readLine()) != null){
+               content += text;
+           }
+
+           modelAndView.addObject("content",content);
+        } else {
+            modelAndView = new ModelAndView("/error.404");
+        }
+        return modelAndView;
+    }
+
 }
